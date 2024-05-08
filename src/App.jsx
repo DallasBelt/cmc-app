b4dbf63b85b570b55e6baa9b4b82a6db09113381
DallasBelt <31676836+DallasBelt@users.noreply.github.com>
2024-05-08 02:36:18 -0500

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './index.css'
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import login from '@/assets/login.jpg'
+import logo from '@/assets/logo.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
+      <img src={logo} className='w-80 my-0 mx-auto py-4'/>
+    
+      <div className='flex flex-col items-center md:flex-row md:justify-center'>
+        <img src={login} alt='' className='w-80 lg:w-2/4 xl:w-2/6 order-2 md:-order-1 mt-5'/>
+        <div className='w-full lg:w-2/4 xl:w-2/6 px-10 space-y-5'>
+          <Input type="email" placeholder='Correo electrónico' className='h-14 text-lg'/>
+          <Input type="password" placeholder='Contraseña' className='h-14 text-lg' />
+          <Button className='w-full h-14 text-xl'>INICIAR SESIÓN</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <Input type="email" placeholder="Email" />
     </>
   )
 }
