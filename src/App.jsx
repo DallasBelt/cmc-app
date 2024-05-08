47645a0949eb3c6cf0a483caeaa13b9f6443e145
dallasrevolt <31676836+DallasBelt@users.noreply.github.com>
2024-05-08 18:10:38 -0500

    fix responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
 
   return (
     <>
-      <img src={logo} className='w-80 my-0 mx-auto py-4'/>
+      <img src={logo} className='w-48 my-5 mx-auto'/>
     
       <div className='flex flex-col items-center md:flex-row md:justify-center'>
-        <img src={login} alt='' className='w-80 lg:w-2/4 xl:w-2/6 order-2 md:-order-1 mt-5'/>
-        <div className='w-full lg:w-2/4 xl:w-2/6 px-10 space-y-5'>
-          <Input type="email" placeholder='Correo electrónico' className='h-14 text-lg'/>
-          <Input type="password" placeholder='Contraseña' className='h-14 text-lg' />
-          <Button className='w-full h-14 text-xl'>INICIAR SESIÓN</Button>
+        <img src={login} alt='' className='hidden lg:block lg:w-2/4 xl:w-2/6'/>
+        <div className='w-full sm:w-3/4 lg:w-2/4 xl:w-2/6 px-10 space-y-5'>
+          <Input type="email" placeholder='Correo electrónico' className='h-14 text-lg' required/>
+          <Input type="password" placeholder='Contraseña' className='h-14 text-lg' required />
+          <Button type='submit' className='w-full h-14 text-xl'>INICIAR SESIÓN</Button>
         </div>
       </div>
     </>
