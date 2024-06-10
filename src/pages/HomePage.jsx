19984134bdeabc3adabb5966e7299ccb9860fef9
dallasrevolt <31676836+DallasBelt@users.noreply.github.com>
2024-06-10 15:20:23 -0500

    Work on the Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,18 +11,71 @@ import {
   NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+import logo from '@/assets/logo.svg';
+
 const Home = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <nav className='p-5 flex justify-between'>
+      <Link to='/'>
+        <img src={logo} className='w-32' />
+      </Link>
+
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Médicos</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Pacientes</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Citas</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Estadísticas</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className='border-transparent hover:border-blue-600 border-2 w-auto h-auto focus:border-blue-600'>
+              <Avatar>
+                <AvatarImage
+                  src='https://github.com/shadcn.png'
+                  alt='@shadcn'
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className='flex flex-col gap-2'>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <a
+                      className='bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                      href='/'
+                    >
+                      Ajustes
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink>
+                    <a className='select-none text-nowrap p-2' href='/'>
+                      Cerrar sesión
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
   );
 };
 
